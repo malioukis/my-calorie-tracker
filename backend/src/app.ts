42f5cbc0acc772310cf175412b7c4b20eaddbdbb
Spyros Malioukis <s.malioukis@gmail.com>
2023-04-02 00:05:26 +0300

    chore: Backend added eslint for ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
-const port: number = 3001;
+const port = 3001;
 
 app.get("/toto", (req: Request, res: Response) => {
   res.send("Hello toto");
